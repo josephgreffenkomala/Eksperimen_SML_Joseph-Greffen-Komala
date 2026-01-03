774cab73b9f9e0ed8577575445305108beabfc0f
Joseph Greffen Komala <josephgreffenkomala@mail.ugm.ac.id>
2026-01-04 05:02:55 +0700

    fix automate

--- preprocessing/automate_AsahDicoding.py
+++ preprocessing/automate_AsahDicoding.py
@@ -5,9 +5,6 @@
 # 1. Import Library
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
